dd6291e2789a9304c8bb1f3954d71477abaffa99
MirthFutures <MirthFutures@gmail.com>
2025-03-20 09:03:29 -0400

    Add addtl support interface

--- contracts/BeefySonic.sol
+++ contracts/BeefySonic.sol
@@ -1043,7 +1043,7 @@ contract BeefySonic is
     /// @return supported True if the contract implements `interfaceId` and
     /// `interfaceId` is not 0xffffffff, false otherwise.
     function supportsInterface(bytes4 interfaceId) external pure returns (bool supported) {
-        if (interfaceId == 0xe3bc4e65 || interfaceId == 0x620ee8e4 || interfaceId == 0x2f0a18c5) return true;
+        if (interfaceId == 0xe3bc4e65 || interfaceId == 0x620ee8e4 || interfaceId == 0x2f0a18c5 || interfaceId == 0x01ffc9a7) return true;
         return false;
     }
 

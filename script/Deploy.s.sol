ed087129fb81f38c66b323ecd4a01f430bb97a8c
MirthFutures <MirthFutures@gmail.com>
2025-03-23 10:43:45 -0400

    Add deploy script

---
+++ script/Deploy.s.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import {Script, console} from "forge-std/Script.sol";
+import {BeefySonic} from "../contracts/BeefySonic.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract CounterScript is Script {
+    address public want = address(0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38);
+    address public stakingContract = address(0xFC00FACE00000000000000000000000000000000);
+    address public beefyFeeRecipient = address(0x02Ae4716B9D5d48Db1445814b0eDE39f5c28264B);
+    address public keeper = address(0x4fED5491693007f0CD49f4614FFC38Ab6A04B619);
+    address public beefyFeeConfig = address(0x2b0C9702A4724f2BFe7922DB92c4082098533c62);
+    address public liquidityFeeRecipient = address(0x10E13f11419165beB0F456eC8a230899E4013BBD);
+    uint256 public liquidityFee = 0.1e18;
+    string public name = "Beefy Sonic";
+    string public symbol = "beS";
+    uint256 public beefyValidatorId = 31;
+    
+    BeefySonic public beefySonic;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        BeefySonic implementation = new BeefySonic();
+        beefySonic = BeefySonic(payable(address(_proxy(address(implementation)))));
+
+        console.log("BeefySonic implementation deployed to:", address(implementation));
+        console.log("BeefySonic deployed to:", address(beefySonic));
+
+        beefySonic.initialize(
+            want,
+            stakingContract,
+            beefyFeeRecipient,
+            keeper,
+            beefyFeeConfig,
+            liquidityFeeRecipient,
+            liquidityFee,
+            name,
+            symbol
+        );
+
+        console.log("BeefySonic initialized");
+
+        beefySonic.addValidator(beefyValidatorId);
+
+        console.log("Beefy validator: 31 added");
+
+        vm.stopBroadcast();
+    }
+
+    function _proxy(address _implementation) internal returns (address) {
+        bytes memory _empty = "";
+        return address(new ERC1967Proxy(address(_implementation), _empty));
+    }
+}
